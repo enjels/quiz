e58de04f1e0f50f33391b11ece6dade7091ea192
Virgílio César <enjels@outlook.com>
2024-02-21 12:30:36 -0300

    problema resolvido na mudança de estado era  nome errado do tipo na mudança?

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,7 +12,7 @@ const Welcome = () => {
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar</p>
-      <button onClick={()=> dispatch({type:"CHANGE_STAGE"})}>Iníciar</button>
+      <button onClick={()=> dispatch({type:"CHANGE_STATE"})}>Iníciar</button>
       <img src={Quiz} alt="Início do quiz" />
     </div>
   );
