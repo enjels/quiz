8479262d94136194fb0621980667576e12e87b0f
Virgílio César <enjels@outlook.com>
2024-02-21 13:13:22 -0300

    problema com randomização das questions comentado para futuramente resolver solução temporaria aplicada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //react, componentes, estáticos
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import Welcome from "./components/Welcome";
 import Questions from "./components/Questions";
@@ -10,11 +10,16 @@ import { QuizContext } from "./context/quiz";
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
 
+  useEffect(() => {
+    //* embaralhar as perguntas
+    dispatch({type:"REORDER_QUESTIONS"})
+  });
+
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      {quizState.gameStage === "Start" && <Welcome/>}
-      {quizState.gameStage === "Playing" && <Questions/>}
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Questions />}
     </div>
   );
 }
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,14 +1,20 @@
-import { useContext } from "react"
-import { QuizContext } from "../context/quiz"
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+import data from "../data/questions";
 
 const Questions = () => {
-    const [quizState, dispatch] = useContext(QuizContext)
-    console.log(quizState)
+  const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion];
   return (
-      <div>
-          Questions
+    <div id="question">
+      <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+          <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
     </div>
-  )
-}
+  );
+};
 
-export default Questions
+export default Questions;
