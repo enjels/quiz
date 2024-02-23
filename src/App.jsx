7a274d342be16773e27419521694a2afd42d1640
Virgílio César <enjels@outlook.com>
2024-02-23 14:26:56 -0300

    adicionado mais um mudança de componente no app, adicionado styeld e personalização do component PickCategory

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
 import Welcome from "./components/Welcome";
+import PickCategory from "./components/PickCategory";
 import Questions from "./components/Questions";
 import GameOver from "./components/GameOver";
 
@@ -20,8 +21,10 @@ function App() {
     <div className="App">
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Questions />}
       {quizState.gameStage === "End" && <GameOver />}
+
     </div>
   );
 }
